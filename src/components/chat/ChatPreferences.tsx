7a75699a4a934ab54bed35e29e90e5e7abcb45e6
Jennifer Ong <jennifer.ex.ong@gmail.com>
2021-02-24 18:59:10 +1300

    create add user to group button

--- src/components/chat/ChatPreferences.tsx
+++ src/components/chat/ChatPreferences.tsx
@@ -1,9 +1,19 @@
+import { useRecoilState } from "recoil";
+import { currentGroupState } from "state";
+
 import styles from "./ChatPreferences.module.scss";
 
 const ChatPreferences: React.FC = () => {
+  const currentGroup = useRecoilState(currentGroupState);
+
+  function handleAddToGroup() {}
+
   return (
     <div className="chatSidePanelContainer">
-      <article className={styles.content}>chat preferences go here</article>
+      <button className={styles.addToGroupButton} onClick={handleAddToGroup}>
+        <i className="fas fa-user-plus"></i>
+        Add user to group
+      </button>
     </div>
   );
 };
