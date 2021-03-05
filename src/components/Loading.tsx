f58212531756bdfc6bcd8b7185510fb13ede9aea
Jennifer Ong <jennifer.ex.ong@gmail.com>
2021-03-06 00:15:43 +1300

    style loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,3 +1,13 @@
-const Loading = () => <div>Loading...</div>;
+import PuffLoader from "react-spinners/PuffLoader";
+
+import styles from "./Loading.module.scss";
+
+const Loading: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <PuffLoader size="10em" color="#2dc2db" />
+    </div>
+  );
+};
 
 export default Loading;
